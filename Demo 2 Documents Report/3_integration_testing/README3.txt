b6b276fc9a929b2bbd4560aa3b93f7ba4edfb968
Song Vu Nguyen <vunnems08@gmail.com>
2018-12-07 10:53:45 -0600

    update integration test

---
+++ Demo 2 Documents Report/3_integration_testing/README3.txt
@@ -0,0 +1,16 @@
+Run integration tests by making sure you have node installed on computer "install node on mac"
+
+The integration test is written by combining unit tests from three components which is create account, login and feed. The map page test code is not working so it will not be part of this demo but will be for the next demo
+
+To run integration test, simply cd to 1_code/Proximity-Angular folder on terminal and type "ng e2e", and the program will automatically run integration test and give you back the result of which test failed and which is pass in the console.
+
+***As for now, the integration tests is not be able to run probably and will give back all failed results, we will try to fix that in the next demo. 
+
+
+This is what we expect to happen if the integration test run probably:
+
+**Step 1: Test the account creation page by going through unit tests for that page. In the end, expect to successfully create a new account with the username equal "test" and password equals "pass"
+
+**Step 2: Test the login page by going through unit tests for that page. In the end, expect login successfully by using the username and password that create in step 1
+
+**Step 3: Test the feed page by going through unit tests for that page. In the end, expect the feed page to successfully loaded feed data from the database
--- Proximity-Angular/e2e/src/app.e2e-spec.ts
+++ Proximity-Angular/e2e/src/app.e2e-spec.ts
@@ -3,11 +3,14 @@
   https://medium.com/paramsingh-66174/automate-e2e-testing-of-angular-4-apps-with-protractorjs-jasmine-fcf1dd9524d5
   https://docs.angularjs.org/guide/e2e-testing
   https://github.com/wallabyjs/public/issues/1768
+  https://www.codeproject.com/Articles/1251554/%2FArticles%2F1251554%2FEnd-to-End-E2E-Tests-in-Angular-Application-Using
 */
 import { AppPage } from './app.po';
 import { CreateAccountComponent } from '../../src/app/create-account/create-account.component';
 import { FeedPageComponent } from '../../src/app/feed-page/feed-page.component';
 import { LoginPageComponent } from '../../src/app/login-page/login-page.component';
+import { EventPageComponent} from '../../src/app/event-page/event-page.component';
+import { MessagingPageComponent} from '../../src/app/messaging-page/messaging-page.component'
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -19,10 +22,11 @@ import { ActivatedRoute, Router } from '@angular/router';
 // Create account page test
 describe('CreateAccountComponent', () => {
   let account: CreateAccountComponent;
-
+  
   it('should create', () => {
     expect(account).toBeTruthy();
   });
+
   describe('onSubmit ', () => {
 
     it('given password and passwordCheck do not match, isValid should be false', () => {
@@ -73,7 +77,7 @@ describe('LoginPageComponent', () => {
 });
 
 
-//map test not working right now, will fix it by demo 2
+//Cannot test map page because of restriction from Google Map API
 
 //feed page test
 describe('FeedPageComponent', () => {
@@ -83,4 +87,23 @@ describe('FeedPageComponent', () => {
     expect(feed).toBeTruthy();
   });
 });
+
+//event page test
+describe('EventPageComponent', () => {
+  let event: EventPageComponent;
+
+  it('should create', () => {
+    expect(event).toBeTruthy();
+  });
+});
+
+//messaging page test
+describe('MessagingPageComponent', () => {
+  let message: MessagingPageComponent;
+
+  it('should create', () => {
+    expect(message).toBeTruthy();
+  });
+});
+
 
